105701ecb0763eef83076b224e0f6d20f829f2a6
hasancelikel <128987370+hasancelikel@users.noreply.github.com>
2023-03-29 03:55:56 +0300

    Update HomepPage.js

--- src/HomepPage.js
+++ src/HomepPage.js
@@ -12,11 +12,7 @@ function HomepPage() {
       <button className='next-page'><Link className='link' to='/App'>Pirinç Analiz</Link></button>
 
       </div>
-      <div className='link-box'>
-
-      <button className='next-page'><Link className='link' to='/Alm'>Alüminyum Analiz</Link></button>    
-
-      </div>
+      
       <input type='text' style={{border:'1px solid black'}}/>
     
     </div>
